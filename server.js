abc03faf03c08d1b3c1be51e1444039f38dcc92a
Adane <adex@2127gmail.com>
2026-09-13 15:55:24 -0700

    Fix CORS for deployed frontend

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const errorHandler = require("./middleware/errorHandler");
 const app = express();
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5500");
+  res.header("Access-Control-Allow-Origin", "https://taskflow-frontend-pql3.onrender.com");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
